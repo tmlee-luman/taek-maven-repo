749fd5abcc1c43a59464abf12c8eb4c63970af2c
tmlee <tmlee@shinsungtk.com>
2022-04-05 11:35:37 +0900

    initial commit

---
+++ src/main/java/FileUploader.java
@@ -0,0 +1,10 @@
+public class FileUploader {
+
+    public String uploadFile() {
+        return "test 입니다.";
+    }
+
+    public void deleteFile() {
+        System.out.println("파일 제거 테스트 입니다.");
+    }
+}
